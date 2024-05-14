aeddb3256bea5d0b7d57a75975f35be8c58aa621
David Griswold <49095734+DavidGriswoldTeacher@users.noreply.github.com>
2024-05-14 13:05:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Getting Started
+
+This is a basic template for java apps. Use it to generate applications that can be used for assignments in java.
---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    
+    public void start() {
+        System.out.println("Hello world!");
+    }
+    
+    
+    public static void main(String[] args) {
+        new App().start();
+    }
+}
